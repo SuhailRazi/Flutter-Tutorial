0a79dd18a69bd2cb9e0148112f49c3f016d7ee01
Suhail Razi P M <suhailrazij@gmail.com>
2023-04-27 21:10:09 +0530

    Sum of 2 numbers

--- basics_dart/bin/basics_dart.dart
+++ basics_dart/bin/basics_dart.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
-void main(){
-  print('hello hii');   // print
+void main() {
+  print("enter two number");
+  var input1 = stdin.readLineSync();
+  var input2 = stdin.readLineSync();
 
-  var a = stdin.readLineSync();  //Input string
-
-  print('hello $a');
-
-  var b = stdin.readLineSync();
-  print("this is $b");
+  var num1 = int.parse(input1!);
+  var num2 = int.parse(input2!);
+  print("Sum = ${num1 + num2}");
 }
